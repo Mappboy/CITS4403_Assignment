c916b76d58135da41c96da3e4530cb3dd03a2ee1
Cameron Poole <Cameron.j.poole@gmail.com>
2014-05-21 17:44:43 +0800

    Another update

--- mySIRmodel.py
+++ mySIRmodel.py
@@ -21,6 +21,7 @@
 # Effect of different values in the agents and model infection rate
 
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as pl
 import sys
 import random
@@ -32,6 +33,10 @@ class Disease(object):
 		self.transmission_rate = transmission_rate
 		self.recovery_time = recovery_time
 		self.r_prob = r_prob
+	def __repr__(self):
+		return "{} disease with beta = {} and gamma = {}".format(self.name,self.transmission_rate,self.r_prob)
+	def __str__(self):
+		return self.__repr__()
 
 class Agent(object):
 	"""Simple agent class for model infected susceptible recovered"""
@@ -66,8 +71,11 @@ class Agent(object):
 
 class SIRmodel(object):
 	"""Creates a simple SIR model object"""
-	def __init__(self, modelname, disease, days=50, susceptible=0.99, infectious=0.01, population=200, d_prob=0.3, death_prob_dis=None, death_prob_norm=None, birth_rate=None ):
+	def __init__(self, modelname, disease, modeltype='SIR', days=50, susceptible=0.9, 
+			infectious=0.1, population=400, d_prob=0.3, graphtype='reg',
+			death_prob_dis=None, death_prob_norm=None, birth_rate=None ):
 		self.name = modelname + disease.name
+		self.disease = disease
 		#Set initial variables for model
 		self.death_prob_dis = death_prob_dis
 		self.death_prob_norm = death_prob_norm
@@ -81,6 +89,7 @@ class SIRmodel(object):
 		self.gamma =  1.0 / float(self.recovery_time) * self.r_prob
 		#probability that in a random graph two nodes will be connected
 		self.d_prob = d_prob
+		self.graphtype = graphtype
 		self.agents = [ Agent(x,self.transmission_rate,self.r_prob) for x in xrange(self.population) ]
 		self.days= days
 		self.finished = 0
@@ -88,35 +97,56 @@ class SIRmodel(object):
 		#Keep track of numbers in each compartment
 		self.susceptibles = [0 for x in range(self.days)]
 		self.infected = [0 for x in range(self.days) ]
+		self.exposed = [0 for x in range(self.days) ]
 		self.recoverers = [0 for x in range(self.days)]
+		#Simulation and graph variables
 		self.diseasenetwork = nx.Graph()
-		self.output = self.run_sim(days)
+		self.isimulations = []
+		self.rsimulations = []
+		self.ssimulations = []
+		self.output = self.run_sim(days,graphtype)
 
 	def randomise_recovery_agents(self):
 		for agent in self.agents:
 			recov_prob = random.random()
 			agent.r_prob = recov_prob
 
+	def init_graph(self,g='reg',k=4):
+		"""Creates a graph of type g"""
+		self.diseasenetwork.add_nodes_from(self.agents)
+		#Rewiring of graphs could count as random_mixing 
+		#Types of random graphs
+		gtype = { 'er':nx.fast_gnp_random_graph(self.population,0.05),
+				'nws':nx.newman_watts_strogatz_graph(self.population,3,self.d_prob),
+				'cws':nx.connected_watts_strogatz_graph(self.population,4,0.7,10),
+				'ba':nx.barabasi_albert_graph(self.population,2),
+				'reg':nx.random_regular_graph(2,self.population),
+				'grid':nx.grid_2d_graph(self.population/2,self.population/2) }
+		#This is causing the trouble need to map each edge to nodes :) 
+		if g == 'grid':
+			self.diseasenetwork.add_edges_from([ (self.agents[x[0]],self.agents[y[0]]) for x,y in gtype[g].edges() ])
+		else:
+			self.diseasenetwork.add_edges_from([ (self.agents[x],self.agents[y]) for x,y in gtype[g].edges() ])
 
-	def run_sim(self, length,start=1, inc=1,g='nws',k=4):
+
+
+	def run_sim(self, length, graphtype, start=1, inc=1):
 		"""Run our simulation.
 		Default will run for a 50 days one day at a time"""
 		if(self.population < 0 ):
 			sys.exit(1)
-		self.diseasenetwork.add_nodes_from(self.agents)
-		#Rewiring of graphs could count as random_mixing 
-		#Types of random graphs
-		gtype = { 'er':nx.fast_gnp_random_graph(self.population,self.d_prob),
-				'nws':nx.newman_watts_strogatz_graph(self.population,3,self.d_prob),
-				'cws':nx.connected_watts_strogatz_graph(self.population,2,self.d_prob),
-				'ba':nx.barabasi_albert_graph(self.population,1),
-				'grid':nx.grid_graph(dim=[self.population/2,self.population/2])}
-		#This is causing the trouble need to map each edge to nodes :) 
-		self.diseasenetwork.add_edges_from([ (self.agents[x],self.agents[y]) for x,y in gtype[g].edges() ])
+		
+		#First graph initialise graph
+		if (len(self.isimulations) == 0):
+			if graphtype:
+				self.init_graph(graphtype)
+			self.init_graph()
+
 		#keep track of compartments
 		sList = [ x for x in self.agents ]
 		iList = []
 		rList = []
+		eList = []
 		for susAgent in sList:
 			if random.random() < self.infectious:
 				if susAgent.infectAgent():
@@ -124,17 +154,18 @@ class SIRmodel(object):
 					sList.remove(susAgent)
 
 		#TODO implement graph for keeping track of infection
-		infecGraph = nx.Graph()
-		infecGraph.add_nodes_from(iList)
-		initialNodes = iList
+		infecTrace = nx.Graph()
+		infecTrace.add_nodes_from(iList)
+		#initialNodes = iList
 		#Should probably randomise infection but just testing
 		#Randomly effect 5% of the population maybe
-		print "Infected/Suscept count to start with : {} : {}  ".format(len(iList),len(sList))
+		print "Infected/Suscept count to start with : {} : {} Graph is {} ".format(len(iList),len(sList),graphtype)
 		while(start <= length and len(iList) != 0):
-			for agent in iList:
+			infected = [ x for x in iList]
+			for agent in infected:
 				for neighbour in self.diseasenetwork.neighbors(agent):
 					if neighbour.susceptible and neighbour.t_prob >= random.random() and neighbour not in iList:
-						infecGraph.add_edge(agent,neighbour)
+						infecTrace.add_edge(agent,neighbour)
 						iList.append(neighbour)
 						sList.remove(neighbour)
 				#Time to recover probability is probably ok 
@@ -145,35 +176,11 @@ class SIRmodel(object):
 				agent.dtime +=1
 
 			if start % 10== 0 :
-
-				pl.clf()
-				pl.figure(figsize=(15,15),dpi=80)
-				susp = [ node for node in self.diseasenetwork.nodes() if node.susceptible ]
-				infec = [ node for node in self.diseasenetwork.nodes() if node.infected and not node.recovered ] 
-				rec = [ node for node in self.diseasenetwork.nodes() if node.recovered ] 
-				#position = nx.circular_layout(self.diseasenetwork)
-				#position = nx.spring_layout(self.diseasenetwork,iterations=100)
-				position = nx.graphviz_layout(self.diseasenetwork,prog='twopi',args='')
-				nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=susp, node_color="b")
-				nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=infec, node_color="r")
-				nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=rec, node_color="g")
-
-				nx.draw_networkx_edges(self.diseasenetwork,position)
-				nx.draw_networkx_labels(self.diseasenetwork,position)
-				cut = 1.00
-   				xmax = cut * max(xx for xx, yy in position.values())
-   				ymax = cut * max(yy for xx, yy in position.values())
-    				pl.xlim(0, xmax)
-   				pl.ylim(0, ymax)
-				nx.draw(self.diseasenetwork)
-				pl.savefig("{0}_SIRmodel_network_{1}.pdf".format(self.name,start))
-				pl.clf()
-				nx.draw(infecGraph,pos=nx.graphviz_layout(infecGraph,prog='twopi',args=''))
-				pl.savefig("{0}_SIRmodel_network_{1}_infection_tree.pdf".format(self.name,start))
-				pl.close()
+				self.plot_graphs(infecTrace,start)
 
 			for neighbour in iList:
 				neighbour.infectAgent()
+
 			self.susceptibles[start-1] = (len(sList)/float(self.population))
 			self.infected[start-1] = (len(iList)/float(self.population))
 			self.recoverers[start-1] = (len(rList)/float(self.population))
@@ -182,8 +189,67 @@ class SIRmodel(object):
 			start+=inc
 		#We can change how we add random stuff later possible part of values
 		#
-		self.finished = start
-		
+		self.finished += start
+		#Increment sim numbers
+		self.isimulations.append(self.infected)
+		self.rsimulations.append(self.recoverers)
+		self.ssimulations.append(self.susceptibles)
+
+	def plot_graphs(self,infecGraph,timep):
+		pl.clf()
+		pl.figure(figsize=(15,15),dpi=80)
+		susp = [ node for node in self.diseasenetwork.nodes() if node.susceptible ]
+		infec = [ node for node in self.diseasenetwork.nodes() if node.infected and not node.recovered ] 
+		rec = [ node for node in self.diseasenetwork.nodes() if node.recovered ] 
+		#position = nx.circular_layout(self.diseasenetwork)
+		position = nx.spring_layout(self.diseasenetwork,iterations=100)
+		#position = nx.graphviz_layout(self.diseasenetwork,prog='twopi',args='')
+		nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=susp, node_color="b")
+		nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=infec, node_color="r")
+		nx.draw_networkx_nodes(self.diseasenetwork, position, nodelist=rec, node_color="g")
+
+		nx.draw_networkx_edges(self.diseasenetwork,position)
+		nx.draw_networkx_labels(self.diseasenetwork,position)
+		#cut = 1.00
+   		#xmax = cut * max(xx for xx, yy in position.values())
+   		#ymax = cut * max(yy for xx, yy in position.values())
+    		#pl.xlim(0, xmax)
+   		#pl.ylim(0, ymax)
+		nx.draw(self.diseasenetwork)
+		#nx.write_dot(self.diseasenetwork,"{0}_SIRmodel_network_{1}.dot".format(self.name,timep))
+		pl.savefig("{0}_SIRmodel_network_{1}.pdf".format(self.name,timep))
+		pl.clf()
+		nx.draw(infecGraph,pos=nx.graphviz_layout(infecGraph,prog='twopi',args=''))
+		pl.savefig("{0}_SIRmodel_network_{1}_infection_tree.pdf".format(self.name,timep))
+		#nx.write_dot(infecGraph,"{0}_infection_tree_{1}.dot".format(self.name,timep))
+		pl.close()
+	def write_graphs(self,infec,timep):
+		pass
+
+	def run_sim_ntimes(self,n):
+		"""Calls run_sim number of times and takes average"""
+		self.clear_arrays()
+		pl.clf()
+		for eachsims in xrange(n):
+			self.run_sim(self.days,self.graphtype)
+		avgrun = self.finished/n
+		pl.clf()
+		print "{} has average run time of {} for {} simulations".format(self.disease,avgrun,n)
+		print self.ssimulations
+		average_infec_sims = np.array(self.isimulations).mean(axis=0)
+		average_recov_sims = np.array(self.rsimulations).mean(axis=0)
+		average_susp_sims = np.array(self.ssimulations).mean(axis=0)
+		pl.plot(average_infec_sims,'-b')
+		pl.plot(average_recov_sims,'-r')
+		pl.plot(average_susp_sims,'-g')
+		pl.savefig("{0}_averages_infection_tree.pdf".format(self.name))
+
+
+	def clear_arrays(self):
+		"""Reset arrays"""
+		self.susceptibles = [0 for x in range(self.days)]
+		self.infected = [0 for x in range(self.days) ]
+		self.recoverers = [0 for x in range(self.days)]
 
 
 	def plot_sim(self):
@@ -212,7 +278,6 @@ class SIRmodel(object):
 	def plot_determ(self,births=False):
 		"""Deterministic plot taken from Modeling infectious diseases"""
 		import scipy.integrate as spi
-		import numpy as np
 		#Taken from 
 		S0=self.susceptible
 		I0=self.infectious
@@ -246,17 +311,22 @@ class SIRmodel(object):
 
 
 def __main__():
+	aids = Disease("aids",transmission_rate=0.6,r_prob=0.4)
 
-	#for graphs in [ 'er','nws','cws','ba','grid']:
-	#	for disease in [ Disease("slowrecov",recovery_time=6), Disease("Badrecov",r_prob=0.1),
-	#			Disease("poortrans", transmission_rate=0.1), Disease("super",transmission_rate=0.9) ]:
-	#		model = SIRmodel("Test"+graphs,disease, population=300)
-	#		model.plot_sim()
-	#		model.plot_determ()
+	#Output Statistics
+	#Clustering coefficient, degree 
+	for graphs in [ 'er','nws','cws','ba','grid','reg']:
+		for disease in [ Disease("slowrecov",recovery_time=6), Disease("Badrecov",r_prob=0.1),
+				Disease("poortrans", transmission_rate=0.1), Disease("super",transmission_rate=0.9) ]:
+			model = SIRmodel("Test"+graphs,disease, population=100,graphtype=graphs)
+			model.run_sim_ntimes(10)
+			#model.init_graph(g=graphs)
+			#model.plot_sim()
+			#model.plot_determ()
 
-	aids = Disease("aids",transmission_rate=0.4)
-	aidsmod = SIRmodel("Test_increased_infecpop",aids,population=200)
-	aidsmod.randomise_recovery_agents()
+	aidsmod = SIRmodel("Test_increased_infecpop",aids,population=50)
+	#aidsmod.randomise_recovery_agents()
+	#aidsmod.run_sim_ntimes(10)
 	aidsmod.plot_sim()
 	aidsmod.plot_determ()
 
